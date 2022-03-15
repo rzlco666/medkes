fb1f7af8958dc3164da1202e01cfb544ba5329bc
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-03-15 19:32:51 +0700

    Fix diagnosa dokter view

--- application/views/dokter/pages/diagnosa.php
+++ application/views/dokter/pages/diagnosa.php
@@ -1,11 +1,8 @@
 <!--  BEGIN CUSTOM STYLE FILE  -->
-<link rel="stylesheet" type="text/css" href="<?= base_url('assets/admin/') ?>plugins/dropify/dropify.min.css">
-<link href="<?= base_url('assets/admin/') ?>assets/css/users/account-setting.css" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('assets/admin/') ?>assets/css/components/custom-modal.css" rel="stylesheet" type="text/css" />
+<link href="<?= base_url('assets/admin/') ?>assets/css/users/account-setting.css" rel="stylesheet" type="text/css"/>
+<link href="<?= base_url('assets/admin/') ?>assets/css/components/custom-modal.css" rel="stylesheet" type="text/css"/>
 <link href="<?= base_url('assets/admin/') ?>plugins/flatpickr/flatpickr.css" rel="stylesheet" type="text/css">
 <link href="<?= base_url('assets/admin/') ?>plugins/flatpickr/custom-flatpickr.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="<?= base_url('assets/admin/') ?>plugins/table/datatable/datatables.css">
-<link rel="stylesheet" type="text/css" href="<?= base_url('assets/admin/') ?>plugins/table/datatable/dt-global_style.css">
 <!--  END CUSTOM STYLE FILE  -->
 <style>
 	.flatpickr-calendar.open {
@@ -13,6 +10,103 @@
 		z-index: 1900;
 	}
 </style>
+
+<div class="content-page">
+	<div class="content">
+
+		<!-- Start Content-->
+		<div class="container-fluid">
+
+			<!-- start page title -->
+			<div class="row">
+				<div class="col-12">
+					<div class="page-title-box">
+						<div class="page-title-right">
+							<ol class="breadcrumb m-0">
+								<li class="breadcrumb-item"><a href="<?= base_url('dokter') ?>">Dashboard</a></li>
+								<li class="breadcrumb-item active">Input Diagnosa</li>
+							</ol>
+						</div>
+						<h4 class="page-title">Input Diagnosa - <?= $id_konsultasi ?></h4>
+					</div>
+				</div>
+			</div>
+			<!-- end page title -->
+
+			<!-- Form row -->
+			<div class="row">
+				<div class="col-12">
+					<div class="card">
+						<div class="card-body">
+							<h4 class="header-title">Diagnosa - <?= $id_konsultasi ?></h4>
+
+							<form class="contact" action="<?= base_url('dokter/diagnosa/proses_tambah_diagnosa/' . $id_konsultasi .'/'.$id_dokter .'/'.$id_pasien) ?>" method="POST" enctype="multipart/form-data">
+								<div class="row mt-3">
+									<div class="col-md-6 mb-3">
+										<label class="form-label">No Rekam Medis</label>
+										<input type="text" name="no_rekam_medis" class="form-control" placeholder="No Rekam Medis" value="<?= set_value('no_rekam_medis') ?>">
+										<span class="form-text text-danger" style="color: red !important;"><?= form_error('no_rekam_medis'); ?></span>
+									</div>
+									<div class="col-md-3 mb-3">
+										<label class="form-label">Tanggal</label>
+										<input id="myDate" name="tanggal" class="form-control flatpickr flatpickr-input active text-black" type="text" placeholder="Pilih Tanggal.." value="<?= set_value('tanggal') ?>">
+										<span class="form-text text-danger"><?= form_error('tanggal'); ?></span>
+									</div>
+									<div class="col-md-3 mb-3">
+										<label class="form-label">Jam</label>
+										<input id="myID" data-enable-time="true" name="jam" class="form-control flatpickr flatpickr-input active text-black" type="text" placeholder="Pilih Jam.." value="<?= set_value('jam') ?>">
+										<span class="form-text text-danger"><?= form_error('jam'); ?></span>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="mb-3 col-md-6">
+										<label class="form-label">Diagnosa</label>
+										<textarea name="diagnosa" class="form-control mb-4" cols="30" rows="8"><?= set_value('diagnosa') ?></textarea>
+										<span class="form-text text-danger"><?= form_error('diagnosa'); ?></span>
+									</div>
+									<div class="mb-3 col-md-6">
+										<label class="form-label">Foto Pemeriksaan</label>
+										<input type="file" name="foto_pemeriksaan" class="form-control mb-4 dropify">
+										<span class="form-text text-danger"><?= form_error('foto_pemeriksaan'); ?></span>
+									</div>
+								</div>
+
+								<button type="submit" class="btn btn-primary waves-effect waves-light">Tambah Diagnosa</button>
+
+							</form>
+
+						</div> <!-- end card-body -->
+					</div> <!-- end card-->
+				</div> <!-- end col -->
+			</div>
+			<!-- end row -->
+
+		</div> <!-- container -->
+
+	</div> <!-- content -->
+
+	<!-- Footer Start -->
+	<footer class="footer">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<script>document.write(new Date().getFullYear())</script> &copy; UBold theme by <a href="">Coderthemes</a>
+				</div>
+				<div class="col-md-6">
+					<div class="text-md-end footer-links d-none d-sm-block">
+						<a href="javascript:void(0);">About Us</a>
+						<a href="javascript:void(0);">Help</a>
+						<a href="javascript:void(0);">Contact Us</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- end Footer -->
+
+</div>
+
 <!-- Modal -->
 <div class="modal fade" id="resep" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered" role="document">
@@ -68,92 +162,25 @@
 		</div>
 	</div>
 </div>
-<!--End Modal -->
-<!--  BEGIN CONTENT AREA  -->
-<div id="content" class="main-content">
-	<div class="layout-px-spacing">
+<!-- end modal -->
 
-		<div class="account-settings-container layout-top-spacing">
+<script src="<?= base_url('assets/admin/') ?>plugins/flatpickr/flatpickr.js"></script>
+<script src="<?= base_url('assets/admin/') ?>plugins/flatpickr/custom-flatpickr.js"></script>
 
-			<div class="account-content">
-				<div class="scrollspy-example" data-spy="scroll" data-target="#account-settings-scroll" data-offset="-100">
-					<!-- Start Diagnosa -->
-					<div class="row">
-						<div class="col-xl-12 col-lg-12 col-md-12 layout-spacing">
-							<form class="section contact" action="<?= base_url('dokter/diagnosa/proses_tambah_diagnosa/' . $id_konsultasi) ?>" method="POST" enctype="multipart/form-data">
-								<div class="info">
-									<h5 class="">Input Diagnosa</h5>
-									<div class="row">
-										<div class="col-md-11 mx-auto">
-											<div class="row">
-												<div class="col-md-6">
-													<div class="form-group">
-														<label>No Rekam Medis</label>
-														<input type="text" name="no_rekam_medis" class="form-control" placeholder="No rekam medis" value="<?= set_value('no_rekam_medis') ?>">
-														<span class="form-text text-danger" style="color: red !important;"><?= form_error('no_rekam_medis'); ?></span>
-													</div>
-												</div>
-												<div class="col-md-3">
-													<div class="form-group">
-														<label>Tanggal</label>
-														<input id="myDate" name="tanggal" class="form-control flatpickr flatpickr-input active text-black" type="text" placeholder="Pilih Tanggal.." value="<?= set_value('tanggal') ?>">
-														<span class="form-text text-danger"><?= form_error('tanggal'); ?></span>
-													</div>
-												</div>
-												<div class="col-md-3">
-													<div class="form-group">
-														<label>Jam</label>
-														<input id="myID" data-enable-time="true" name="jam" class="form-control flatpickr flatpickr-input active text-black" type="text" placeholder="Pilih Jam.." value="<?= set_value('jam') ?>">
-														<span class="form-text text-danger"><?= form_error('jam'); ?></span>
-													</div>
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-md-6">
-													<div class="form-group">
-														<label>Diagnosa</label>
-														<textarea name="diagnosa" class="form-control mb-4" cols="30" rows="8"><?= set_value('diagnosa') ?></textarea>
-														<span class="form-text text-danger"><?= form_error('diagnosa'); ?></span>
-													</div>
-												</div>
-												<div class="col-md-6">
-													<div class="form-group">
-														<label>Foto Pemeriksaan</label>
-														<input type="file" name="foto_pemeriksaan" class="form-control mb-4 dropify">
-														<span class="form-text text-danger"><?= form_error('foto_pemeriksaan'); ?></span>
-													</div>
-												</div>
-											</div>
-											<button class="btn btn-primary mb-2">Tambah Diagnosa</button>
-										</div>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-					<!-- End Diagnosa -->
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<script src="<?= base_url('assets/admin/') ?>plugins/flatpickr/flatpickr.js"></script>
-	<script src="<?= base_url('assets/admin/') ?>plugins/flatpickr/custom-flatpickr.js"></script>
-
-	<script>
-		flatpickr("#myDate", {
-			enableTime: true,
-			dateFormat: "Y-m-d",
-			defaultDate: "today",
-			clickOpens: false
-		});
-	</script>
-	<script>
-		flatpickr("#myID", {
-			enableTime: true,
-			noCalendar: true,
-			dateFormat: "H:i",
-			defaultDate: new Date(),
-			clickOpens: false
-		});
-	</script>
+<script>
+	flatpickr("#myDate", {
+		enableTime: true,
+		dateFormat: "Y-m-d",
+		defaultDate: "today",
+		clickOpens: false
+	});
+</script>
+<script>
+	flatpickr("#myID", {
+		enableTime: true,
+		noCalendar: true,
+		dateFormat: "H:i",
+		defaultDate: new Date(),
+		clickOpens: false
+	});
+</script>
